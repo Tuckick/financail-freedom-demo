852ccb276bbf41741b8ab54689be1a72a6ad036b
Tuck <tuk.pensrisirikul@hotmail.com>
2021-09-12 16:15:46 +0700

    add route to career

--- components/header.vue
+++ components/header.vue
@@ -18,7 +18,9 @@
                     <b-nav-item href="/blog">BLOG</b-nav-item>
                 </nuxt-link>
                 <b-nav-item-dropdown text="CONTACT" right>
+                <nuxt-link to="/career">
                     <b-dropdown-item href="/career">CAREER</b-dropdown-item>
+                </nuxt-link>
                 </b-nav-item-dropdown>
             </b-navbar-nav>
         </b-navbar>
--- pages/Career.vue
+++ pages/Career.vue
@@ -27,7 +27,7 @@
 </template>
 <script>
 export default {
-    
+    name: 'Career'
 }
 </script>
 <style>
